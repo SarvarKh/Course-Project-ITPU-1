c9c5b9398e8220e846dbdc7b0853312bf2dfc318
Sarvar Khalimov <khalimovsarvar@gmail.com>
2023-05-29 10:45:07 +0500

    Refactor toString() methods of sub-classess

--- src/main/java/org/example/domain/Bedclothing.java
+++ src/main/java/org/example/domain/Bedclothing.java
@@ -1,15 +1,12 @@
 package org.example.domain;
 
-import org.example.domain.parameters.BeddingSize;
-import org.example.domain.parameters.FillingMaterial;
-import org.example.domain.parameters.Material;
-import org.example.domain.parameters.Style;
+import org.example.domain.parameters.*;
 
 public class Bedclothing extends Inventory {
     private final BeddingSize beddingSize;
     private final FillingMaterial fillingMaterial;
 
-    public Bedclothing(int id, long price, int weight, Material material, String color,
+    public Bedclothing(int id, long price, int weight, Material material, Color color,
                        Style style, BeddingSize beddingSize, FillingMaterial fillingMaterial) {
         super(id, price, weight, material, color, style);
         this.beddingSize = beddingSize;
@@ -27,6 +24,12 @@ public class Bedclothing extends Inventory {
     @Override
     public String toString() {
         return "Bedclothing{" +
+                "id=" + this.getId() +
+                ", price=" + this.getPrice() +
+                ", weight=" + this.getWeight() +
+                ", material=" + this.getMaterial() +
+                ", color=" + this.getColor() +
+                ", style=" + this.getStyle() +
                 "beddingSize=" + beddingSize +
                 ", fillingMaterial=" + fillingMaterial +
                 '}';
--- src/main/java/org/example/domain/Dish.java
+++ src/main/java/org/example/domain/Dish.java
@@ -7,7 +7,7 @@ public class Dish extends Inventory {
     private final HeatResistance heatResistance;
     private final DishSize dishSize;
 
-    public Dish(int id, long price, int weight, Material material, String color, Style style,
+    public Dish(int id, long price, int weight, Material material, Color color, Style style,
                 FoodSafety foodSafety, HeatResistance heatResistance, DishSize dishSize) {
         super(id, price, weight, material, color, style);
         this.foodSafety = foodSafety;
@@ -26,6 +26,12 @@ public class Dish extends Inventory {
     @Override
     public String toString() {
         return "Dish{" +
+                "id=" + this.getId() +
+                ", price=" + this.getPrice() +
+                ", weight=" + this.getWeight() +
+                ", material=" + this.getMaterial() +
+                ", color=" + this.getColor() +
+                ", style=" + this.getStyle() +
                 "foodSafety=" + foodSafety +
                 ", heatResistance=" + heatResistance +
                 '}';
