0836064417a94d70d54e0b0ab175a9a18acd36a6
Sarvar Khalimov <khalimovsarvar@gmail.com>
2023-05-30 11:21:25 +0500

    Make console interactive to get all or specific inventory

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,38 +3,85 @@ package org.example;
 import org.example.dao.InventoryDao;
 import org.example.dao.impl.InventoryCsvBasedDaoImpl;
 import org.example.dto.Inventory;
+import org.example.dto.parameters.Color;
 import org.example.service.InventoryService;
 import org.example.service.impl.InventoryServiceImpl;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Starting application...");
         InventoryDao dao = new InventoryCsvBasedDaoImpl();
         InventoryService service = new InventoryServiceImpl(dao);
+        List<String> userInput = getUserInput();
 
-        if (args.length == 0){
+        if (userInput.size() == 0){
             System.out.println("There were no filtering params.");
 
             printAllDataFromTheFile(service);
         } else {
-            String filteringParam = fetchFilteringParam(args);
-            System.out.println("Industry code for filtering is "+ filteringParam);
+            String filteringParam = fetchFilteringParam(userInput);
+            System.out.println("Color for filtering is "+ filteringParam);
 
-            printFilteredDataByIndustryCode(service, filteringParam);
+            printFilteredDataByColor(service, filteringParam);
         }
     }
 
-    private static String fetchFilteringParam(String[] args) {
-        String filteringParam = String.valueOf(args[0]);
+    private static List<String> getUserInput() throws IOException {
+        System.out.println("Please, enter 1 to retrieve all inventory,");
+        System.out.println("Or enter 2 to retrieve only specific inventories by color");
+
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(System.in));
+        String input = reader.readLine();
+
+        List<String> result = new ArrayList<>();
+        if (input.equals("1")) {
+            return result; // empty list
+        }
+
+        while (!input.equals("2")) {
+            System.out.println("\n!WARNING: You entered invalid number, please only enter 1 or 2");
+            getUserInput();
+        }
+
+        input = selectColor();
+        result.add(input);
+        return result;
+    }
+
+    private static String selectColor() throws IOException {
+        System.out.println("Please, enter corresponding number to retrieve only specific inventories by its color");
+        int index = 0;
+        for (Color color : Color.values()) {
+            System.out.println(""+index+" - " + color);
+            index += 1;
+        }
+
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(System.in));
+        String input = reader.readLine();
+        if (!input.matches("^(10|[0-9])$")) {
+            System.out.println("\n!WARNING: You entered invalid number, please only enter numbers between 0 or 10");
+            selectColor();
+        }
+        return Color.values()[Integer.parseInt(input)].toString();
+    }
+
+    private static String fetchFilteringParam(List<String> args) {
+        String filteringParam = String.valueOf(args.get(0));
         return filteringParam;
     }
 
-    private static void printFilteredDataByIndustryCode(InventoryService service, String filteringParam) {
-        List<Inventory> surveyData = service.retrieveInventoryDataFilteredByColor(filteringParam);
+    private static void printFilteredDataByColor(InventoryService service, String filteringParam) {
+        List<Inventory> inventoryData = service.retrieveInventoryDataFilteredByColor(filteringParam);
 
-        for (Inventory item : surveyData) {
+        for (Inventory item : inventoryData) {
             System.out.println(item);
         }
     }
